a5cba0f745691c97ecd097618f5f305cb5dbe55b
윤희상 <smartai.heethangee@gmail.com>
2025-11-20 12:05:03 +0900

    chore: document voice package exports

--- app/services/voice/__init__.py
+++ app/services/voice/__init__.py
@@ -1,3 +1,4 @@
+# 음성 서비스 모듈에서 제공하는 STT/TTS 클래스들을 외부로 노출한다.
 from .stt import OpenAIWhisperSTT, STTError, TranscriptionResult
 from .tts import OpenAITTSEngine, TTSError, TextToSpeechService, TTSResult
 
